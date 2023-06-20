b91386d91bc689fdf570ce5b0d1aae76c3312a6a
Oceanpanda007 <aaronessuman@gmail.com>
2023-06-20 16:16:16 -0700

    Tasks on functions, nested loops, and headers in C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,11 @@
 int main(void)
 {
 	int i;
-	char word [] = "_putchar";
-	
-	for(i = 0 ; i < 8 ; i++)
+	char word[] = "_putchar";	
+	for (i = 0 ; i < 8 ; i++)
 	{
 		_putchar(word[i]);
 	}
-	
 	_putchar('\n');	
-	
 	return (0);
 }
