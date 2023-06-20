27424b1b0745802147195bd1f00cc44fb20bc37e
Oceanpanda007 <aaronessuman@gmail.com>
2023-06-20 16:45:08 -0700

    Tasks on functions, nested loops, and headers in C correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *Program 2-print_alphabet_x10 Prints 10 times the alphabet in lowercase.
+ *Function print_alphabet_x10 Prints 10 times the alphabet in lowercase.
  *
  */
 
