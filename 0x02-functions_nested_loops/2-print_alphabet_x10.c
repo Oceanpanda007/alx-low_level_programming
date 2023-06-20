6279c009cc2d1d4a1205325ac7f0ee076846538d
Oceanpanda007 <aaronessuman@gmail.com>
2023-06-20 16:36:27 -0700

    Tasks on functions, nested loops, and headers in C

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,12 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for(i = 0 ;i < 10 ; i++)
+	for (i = 0 ;i < 10 ; i++)
 	{
-		for(j = 97 ; j < 123 ; j++)
+		for (j = 97 ; j < 123 ; j++)
 		{
 			_putchar(j);
 		}
-		_putchar('\n');	
+		_putchar('\n');
 	}
-}	
+}
