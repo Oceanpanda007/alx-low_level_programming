74fb015ed8368499b51a929085621cfe3136271c
Oceanpanda007 <aaronessuman@gmail.com>
2023-06-20 18:41:58 -0700

    Tasks on functions, nested loops, and headers in C corrections

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,33 +5,38 @@
 * Return: Always 0.
 */
 
-void times_table(void)
-{
-	int a;
-	int b;
-	int c;
+void times_table(void) {
+    int a;
+    int b;
+    int c;
 
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = 0; b <= 9; b++)
+    for (a = 0; a <= 9; a++) {
+        for (b = 0; b <= 9; b++) {
+            c = a * b;
+
+            if (c <= 9)
+	    {
+                _putchar(48 + c);
+                if (b < 9)
 		{
-			c = a * b;
-			if (c <= 9)
-			{
-			_putchar(48 + c);
-			_putchar(44);
-			_putchar(32);
-			_putchar(32);
-			}
-			else
-			{
-			_putchar((c / 10) + 48);
-			_putchar((c % 10) + 48);
-			_putchar(44);
-			_putchar(32);
-			}
+                    _putchar(44);
+                    _putchar(32);
+                    _putchar(32);
+                }
+            }
+	    else
+	    {
+                _putchar((c / 10) + 48);
+                _putchar((c % 10) + 48);
+                if (b < 9)
+	       	{
+                    _putchar(44);
+                    _putchar(32);
+                }
+            }
+        }
 
-		}
-		_putchar(10);
-	}
+        _putchar(10);
+    }
 }
+
