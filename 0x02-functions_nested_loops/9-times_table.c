93eaeee07c96cafa665e3a7e73e4e5386c285aed
Oceanpanda007 <aaronessuman@gmail.com>
2023-06-20 18:25:11 -0700

    Tasks on functions, nested loops, and headers in C corrections

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,6 @@ void times_table(void) {
 		{
                     _putchar(44);
                     _putchar(32);
-		    _putchar(32);
 		}
             }
 	    else
@@ -32,6 +31,7 @@ void times_table(void) {
                 _putchar((c % 10) + 48);
                 if (b < 9) {
                     _putchar(44);
+		    _putchar(32);
                 }
             }
         }
