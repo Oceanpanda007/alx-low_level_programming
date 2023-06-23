132ef0991fcf606e57ed11cb85bc02aa906d7d59
Oceanpanda007 <aaronessuman@gmail.com>
2023-06-22 18:35:37 -0700

    Advanced task that prints integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,15 +13,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		n*= -1;
+		n *= -1;
 		k = n;
 		_putchar('-');
 	}
 	k /= 10;
-	
 	if (k != 0)
 	print_number(k);
 	_putchar((unsigned int) n % 10 + '0');
-
-
 }
