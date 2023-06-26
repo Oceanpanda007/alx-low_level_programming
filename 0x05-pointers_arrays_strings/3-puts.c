f15c4a32d4c58ca75313d3dcff193a0d1fdfcd21
Oceanpanda007 <aaronessuman@gmail.com>
2023-06-26 16:28:46 -0700

    Puts. A function that prints a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,9 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
  *_puts - prints a string, followed by a line, to stdout
  *@str: characters for the string to be printed.
- *Return: not.
+ *Return: nothing.
  */
 void _puts(char *str)
 {
